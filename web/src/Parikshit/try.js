b7599f1cef895eb41f13395287202c7281d41496
Parikshit Diwan <parikshitdiwan@Parikshits-MacBook-Pro.local>
2021-10-18 00:33:57 +1100

    select added and reference corrected

--- web/src/Parikshit/try.js
+++ web/src/Parikshit/try.js
@@ -1,18 +1,24 @@
 var mydata = JSON.parse(data)[0];
 var ctx = document.getElementById('timeSeriesChart');
+var city= document.getElementById("city").value;
 
 function getCityDataWasteType(data,city,wasteType){
   let cityData=data[city];
+  let referenceYear=[];
+  for(let i=0;i<Object.keys(cityData).length;i++){
+    referenceYear.push(Object.keys(cityData)[i].split("-")[0]);
+  }
   let referenceYears=Object.keys(cityData);
   let garbageData=[];
   for(let i=0;i<referenceYears.length;i++){
     garbageData.push(Number(cityData[referenceYears[i]][wasteType].replace(",","")));
   }
-  return[referenceYears,garbageData];
+  return[referenceYear,garbageData];
 }
 
-var [referenceYears,recycleCollectedData]=getCityDataWasteType(mydata,"Knox","recycle_collected");
-var [referenceYears1,recycleProcessedData]=getCityDataWasteType(mydata,"Knox","recycle_processed");
+
+var [referenceYears,recycleCollectedData]=getCityDataWasteType(mydata,city,"recycle_collected");
+var [referenceYears1,recycleProcessedData]=getCityDataWasteType(mydata,city,"recycle_processed");
 
 var chart = new Chart(ctx, {
   type: 'line',
@@ -71,3 +77,29 @@ var chart = new Chart(ctx, {
     }
   }
 });
+
+function changeChart(){
+  let city = document.getElementById("city").value;
+  let [referenceYears,recycleCollectedData]=getCityDataWasteType(mydata,city,"recycle_collected");
+  let [referenceYears1,recycleProcessedData]=getCityDataWasteType(mydata,city,"recycle_processed");
+  chart.data.datasets=[{
+    label: 'Recycle Collected',
+    data: recycleCollectedData,
+    borderWidth: 3,
+    borderColor:'rgb(0, 0, 255)',
+    backgroundColor:'rgb(0, 0, 255)'
+  },{
+    label: 'Recycle Processed',
+    data: recycleProcessedData,
+    borderWidth: 3,
+    borderColor:'rgb(0, 255, 0)',
+    backgroundColor:'rgb(0, 255, 0)',
+    fill: {
+      target: '-1',
+      above: 'rgb(255, 255, 0)',   // Area will be red above the origin
+      below: 'rgb(255, 0, 0)'    // And blue below the origin
+    }
+  }];
+  chart.update();
+
+}
